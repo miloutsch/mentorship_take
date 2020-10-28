ded0e0fb06bbde2505629270c7b19436e1d7d44d
Milo Utsch <milo@take.net>
2020-10-28 14:55:04 -0300

    adding project structure

---
+++ euler/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Milo Utsch'
+__version__ = '0.1.0'
+__license__ = 'MIT License'
+__credits__ = ['Milo Utsch', 'Gabriel Salgado']
---
+++ setup.py
@@ -0,0 +1,3 @@
+__author__ = 'Milo Utsch'
+__version__ = '0.1.0'
+
