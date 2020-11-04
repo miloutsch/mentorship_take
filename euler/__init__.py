94a74abf272e4be29fa15185d64a2835a99851ff
milo <milo.utsch@gmail.com>
2020-11-04 15:01:23 -0200

    Structuring setup file.

---
+++ euler/__init__.py
@@ -0,0 +1,13 @@
+"""Solutions for Euler Project.
+
+Euler Project is in https://projecteuler.net/
+This project intents to resolve each question from the Euler Project.
+"""
+
+__author__ = 'Milo Utsch'
+__email__ = 'milo.utsch@gmail.com'
+__version__ = '0.1.0'
+__license__ = 'MIT License'
+__credits__ = ['Milo Utsch', 'Gabriel Salgado']
+
+
